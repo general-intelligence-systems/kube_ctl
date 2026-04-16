e1965a80c657be3be83973cdad74505eb62d5270
Nathan Kidd <nathankidd@hey.com>
2026-04-16 11:55:26 +0100

    initial gem

--- test/kube_ctl_test.rb
+++ test/kube_ctl_test.rb
@@ -2,8 +2,8 @@
 
 require "test_helper"
 
-class GemKitTest < Minitest::Test
+class KubeCtlTest < Minitest::Test
   def test_version
-    refute_nil GemKit::VERSION
+    refute_nil KubeCtl::VERSION
   end
 end
